99564c729f5bad753ee53faed776e6b0cfebd2a0
satochu40-lang <satochu.40@gmail.com>
2025-09-15 21:39:26 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "messages#index"
-  resources :users, only: [:edit, :update]  
+  resources :users, only: [:edit, :update] 
+  resources :rooms, only: [:new, :create]  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
