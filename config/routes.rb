f6e409e271a025a8782de2eecc7dc3f9f5f0f196
satochu40-lang <satochu.40@gmail.com>
2025-09-15 18:27:53 +0900

    ユーザー編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'messages/index'
+  root to: "messages#index"
+  resources :users, only: [:edit, :update]  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
